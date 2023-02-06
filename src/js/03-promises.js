0fef038508302b7c82475c1f43c079566e71c4cf
Andriy Bodnar <sokilets3@gmail.com>
2023-02-06 12:45:52 +0200

    1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,7 +4,6 @@ const formEl = document.querySelector('.form');
 const delayEl = document.getElementsByName('delay');
 const stepEl = document.getElementsByName('step');
 const amountEl = document.getElementsByName('amount');
-console.log('hi');
 formEl.addEventListener('submit', getSubmitForm);
 
 function getSubmitForm(e) {
@@ -22,7 +21,6 @@ function getSubmitForm(e) {
           `❌ Rejected promise ${position} in ${delay}ms`
         );
       });
-    console.log(delay);
     delay += Number(stepEl[0].value);
   }
   formEl.reset();
